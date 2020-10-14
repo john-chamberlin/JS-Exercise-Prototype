89826ec9a68e7e333e2e810c20aa48846d5c8db8
John Chamberlin <johnchamberlin@Johns-MBP.hsd1.wa.comcast.net>
2020-10-14 12:30:52 -0700

    mvp done

--- index.js
+++ index.js
@@ -39,8 +39,23 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(paramName, paramAge,) {
+  this.name = paramName;
+  this.age = paramAge;
+  this.stomach = [];
 
+}
+Person.prototype.eat = function(food){
+  if(this.stomach.length < 10){
+    this.stomach.push(food)
+  } else return
+  
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
 }
 
 /*
@@ -57,8 +72,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(paramModel, paramMPG) {
+  this.model = paramModel;
+  this.milesPerGallon = paramMPG;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank += gallons
 }
 
 /*
@@ -68,18 +89,28 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(paramName, paramAge, paramFavToy) {
+  Person.call(this, paramName, paramAge);
+  this.favoriteToy = paramFavToy;
+}
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function(){
+  return `playing with ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+
+  1. implicit binding is basically a scope where the 'this' keyword is is implied given the context it's used in.
+
+  2. explicit binding is when you explicitly declare what the 'this' keyword is referring to. 
+
+  3. global binding is when the 'this' keyword isn't referenced or defined in any way, meaning that it references the overall code of javascript.
+
+  4.  new binding occurs when you create something using a constructor function, it automatically assumes that the 'this' in the contructor function is the same 'this' as in the thing you're creating. 
 */
 
 
